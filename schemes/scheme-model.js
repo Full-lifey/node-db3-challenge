31d94b7b96c54e6fc49a02fc3c7039f55b87c3ee
Full-lifey <44554249+Full-lifey@users.noreply.github.com>
2019-07-17 15:54:06 -0600

    findSteps scheme model completed

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,7 +2,8 @@ const db = require('../data/db-config.js');
 
 module.exports = {
   find,
-  findById
+  findById,
+  findSteps
 };
 
 function find() {
@@ -10,4 +11,29 @@ function find() {
   return db('schemes');
 }
 
-function findById(id) {}
+function findById(id) {
+  return db('schemes')
+    .where({ id })
+    .first()
+    .then(scheme => {
+      if (scheme) {
+        return scheme;
+      } else {
+        return null;
+      }
+    })
+    .catch(err => null);
+}
+
+function findSteps(id) {
+  console.log(id);
+  return db('schemes')
+    .innerJoin('steps', 'schemes.id', 'steps.scheme_id')
+    .where({ 'schemes.id': id })
+    .select('schemes.scheme_name', 'steps.step_number', 'steps.instructions');
+}
+
+/*SELECT scheme_name, step_number, instructions
+FROM schemes
+INNER JOIN steps on schemes.id = steps.id
+WHERE schemes.id = 2 */
